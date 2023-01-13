b938a32bd21850cf39d4a4e5db5310b740620964
Edward McCready <EdwardMcCready@hotmail.co.uk>
2023-01-13 01:58:33 +0000

    Add project files.

---
+++ FinanceApplicationAPI/Controllers/FinanceController.cs
@@ -0,0 +1,26 @@
+﻿using FinanceApplicationAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceApplicationAPI.Controllers
+{
+    [Route("Finance")]
+    [Controller]
+    public class FinanceController : Controller
+    {
+        [HttpGet]
+        [Route("Test")]
+        public TransactionDataModel GetTest()
+        {
+            var test = new TransactionDataModel
+            {
+                Amount = 1,
+                DateTime = DateTime.Now,
+                Description = "Test",
+                Type = false,
+                Year = 1
+            };
+
+            return test;
+        }
+    }
+}
---
+++ FinanceApplicationAPI/Models/TransactionDataModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FinanceApplicationAPI.Models
+{
+    public class TransactionDataModel
+    {
+        public DateTime DateTime { get; set; }
+        public int Year { get; set; }
+        public string? Description { get; set; }
+
+        public bool Type { get; set; }
+        public double Amount { get; set; }
+    }
+}
