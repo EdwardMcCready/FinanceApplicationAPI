2a959069fc0d0cfad1c8ae841993e0eac3f09742
Edward McCready <EdwardMcCready@hotmail.co.uk>
2023-01-14 19:24:33 +0000

    Fixced spacing

--- FinanceApplicationAPI/DataAccess/Models/Transaction.cs
+++ FinanceApplicationAPI/DataAccess/Models/Transaction.cs
@@ -5,13 +5,10 @@ namespace FinanceApplicationAPI.Data.Models
     public class Transaction
     {
         public string? TransactionID { get; set; }
-
         public DateTime Date { get; set; }
         public string? Description { get; set; }
-
         public bool TransactionType { get; set; }
         public string? Type { get; set; }
-
         public double Amount { get; set; }
 
         public string? UserID { get; set; }
